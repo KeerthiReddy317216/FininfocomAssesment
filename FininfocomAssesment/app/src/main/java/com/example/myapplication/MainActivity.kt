6e58da417c20e0f0c3f0af23e545d2b90f7624b6
kreerthi <keerthireddy@123>
2025-02-02 01:19:38 +0530

    final commit

--- FininfocomAssesment/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ FininfocomAssesment/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,83 +1,60 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var numberAdapter: NumberAdapter
-    private val numbersData:MutableList<Int> = mutableListOf()
+    private val numbersData: MutableList<Int> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        for (i in 1..100){
+        for (i in 1..100) {
             numbersData.add(i)
         }
-        val layoutManager:GridLayoutManager = GridLayoutManager(applicationContext,10)
+        val layoutManager = GridLayoutManager(applicationContext, 10)
         binding.numbersGrid.layoutManager = layoutManager
-        numberAdapter = NumberAdapter(numbersData,"",applicationContext)
+        numberAdapter = NumberAdapter(numbersData, "", applicationContext)
         binding.numbersGrid.adapter = numberAdapter
 
-        val onClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                when(v?.id){
-                    R.id.get_prime_number ->{
-                        numberAdapter = NumberAdapter(numbersData,applicationContext.resources.getString(R.string.prime_target_path),applicationContext)
-                        binding.numbersGrid.adapter = numberAdapter
-
-                    }
-                    R.id.get_even_number ->{
-                        numberAdapter = NumberAdapter(numbersData,applicationContext.resources.getString(R.string.even_target_path),applicationContext)
-                        binding.numbersGrid.adapter = numberAdapter
-
-                    }
-                    R.id.get_odd_numbers ->{
-                        numberAdapter = NumberAdapter(numbersData,applicationContext.resources.getString(R.string.odd_target_path),applicationContext)
-                        binding.numbersGrid.adapter = numberAdapter
-
-                    }
-                    R.id.get_fibonacci_sequence ->{
-                        numberAdapter = NumberAdapter(numbersData,applicationContext.resources.getString(R.string.fibonacci_target_path),applicationContext)
-                        binding.numbersGrid.adapter = numberAdapter
-
-                    }
-                    R.id.remove_filter ->{
-                        numberAdapter = NumberAdapter(numbersData,"",applicationContext)
-                        binding.numbersGrid.adapter = numberAdapter
-
-                    }
-
+        val onClickListener: View.OnClickListener = View.OnClickListener { v ->
+            when (v?.id) {
+                R.id.get_prime_number -> {
+                    numberAdapter.setNumber(applicationContext.getString(R.string.prime_target_path))
                 }
 
+                R.id.get_even_number -> {
+                    numberAdapter.setNumber(applicationContext.getString(R.string.even_target_path))
+                }
+
+                R.id.get_odd_numbers -> {
+                    numberAdapter.setNumber(applicationContext.getString(R.string.odd_target_path))
+                }
+
+                R.id.get_fibonacci_sequence -> {
+                    numberAdapter.setNumber(applicationContext.getString(R.string.fibonacci_target_path))
+                }
+
+                R.id.remove_filter -> {
+                    numberAdapter.setNumber("")
+                }
             }
-
         }
-        binding.getEvenNumber.setOnClickListener(onClickListener)
-        binding.getOddNumbers.setOnClickListener(onClickListener)
-        binding.getFibonacciSequence.setOnClickListener(onClickListener)
-        binding.getPrimeNumber.setOnClickListener(onClickListener)
-        binding.removeFilter.setOnClickListener(onClickListener)
+        binding.apply {
+            getEvenNumber.setOnClickListener(onClickListener)
+            getOddNumbers.setOnClickListener(onClickListener)
+            getFibonacciSequence.setOnClickListener(onClickListener)
+            getPrimeNumber.setOnClickListener(onClickListener)
+            removeFilter.setOnClickListener(onClickListener)
+        }
     }
-
 
 
 }
--- FininfocomAssesment/app/src/main/java/com/example/myapplication/NumberAdapter.kt
+++ FininfocomAssesment/app/src/main/java/com/example/myapplication/NumberAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,10 +8,14 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NumberAdapter(val numbersList:List<Int>, val targetPath: String,val context: Context):
+class NumberAdapter(
+    private val numbersList: List<Int>,
+    private var targetPath: String,
+    private val context: Context
+) :
     RecyclerView.Adapter<NumberAdapter.NumberViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.number_item,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.number_item, parent, false)
         return NumberViewHolder(view)
     }
 
@@ -22,49 +25,64 @@ class NumberAdapter(val numbersList:List<Int>, val targetPath: String,val contex
 
     override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
         holder.numberText.text = numbersList[position].toString()
+        holder.numberLayout.setBackgroundColor(context.getColor(R.color.white))
+        when (targetPath) {
+            context.getString(R.string.prime_target_path) -> {
+                if (isPrime(numbersList[position])) {
+                    holder.numberLayout.setBackgroundColor(context.getColor(R.color.action_prime_bg_color))
+                }
+            }
 
-        if(targetPath.equals(context.resources.getString(R.string.prime_target_path))){
-            if(isPrime(numbersList[position])){
-                holder.numberLayout.setBackgroundColor(context.resources.getColor(R.color.action_prime_bg_color))
+            context.getString(R.string.even_target_path) -> {
+
+                if (numbersList[position] % 2 == 0) {
+                    holder.numberLayout.setBackgroundColor(context.getColor(R.color.action_even_bg_color))
+                }
             }
-        } else if(targetPath.equals(context.resources.getString(R.string.even_target_path))){
-            if(numbersList[position] % 2 == 0){
-                holder.numberLayout.setBackgroundColor(context.resources.getColor(R.color.action_even_bg_color))
+
+            context.getString(R.string.odd_target_path) -> {
+                if (numbersList[position] % 2 != 0) {
+                    holder.numberLayout.setBackgroundColor(context.getColor(R.color.action_odd_bg_color))
+                }
             }
-        } else if(targetPath.equals(context.resources.getString(R.string.odd_target_path))){
-            if(numbersList[position] % 2 != 0){
-                holder.numberLayout.setBackgroundColor(context.resources.getColor(R.color.action_odd_bg_color))
-            }
-        } else if(targetPath.equals(context.resources.getString(R.string.fibonacci_target_path))){
-            if(isFibonacci(numbersList[position])){
-                holder.numberLayout.setBackgroundColor(context.resources.getColor(R.color.action_fibonacci_bg_color))
+
+            context.getString(R.string.fibonacci_target_path) -> {
+                if (isFibonacci(numbersList[position])) {
+                    holder.numberLayout.setBackgroundColor(context.getColor(R.color.action_fibonacci_bg_color))
+                }
             }
         }
+
     }
-    fun isPrime(number: Int): Boolean {
+
+    fun setNumber(targetPathChange: String) {
+        targetPath = targetPathChange
+        notifyItemRangeChanged(0, numbersList.size)
+    }
+
+    private fun isPrime(number: Int): Boolean {
         if (number <= 1) {
-            return false  // Numbers less than or equal to 1 are not prime
+            return false
         }
         if (number == 2) {
-            return true  // 2 is a prime number
+            return true
         }
         if (number % 2 == 0) {
-            return false  // Even numbers greater than 2 are not prime
+            return false
         }
-
-        // Check odd divisors from 3 up to sqrt(number)
         var divisor = 3
         while (divisor * divisor <= number) {
             if (number % divisor == 0) {
-                return false  // Found a divisor, not a prime number
+                return false
             }
-            divisor += 2  // Check next odd number
+            divisor += 2
         }
 
-        return true  // No divisors found, it's a prime number
+        return true
     }
-    fun isFibonacci(number: Int): Boolean {
-        if (number < 0) return false  // Fibonacci sequence starts from 0
+
+    private fun isFibonacci(number: Int): Boolean {
+        if (number < 0) return false
 
         var a = 0
         var b = 1
@@ -78,7 +96,7 @@ class NumberAdapter(val numbersList:List<Int>, val targetPath: String,val contex
     }
 
 
-    inner class NumberViewHolder(val itemView: View): RecyclerView.ViewHolder(itemView) {
+    inner class NumberViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val numberLayout: LinearLayout = itemView.findViewById(R.id.number_item)
         val numberText: TextView = itemView.findViewById(R.id.digit)
     }
